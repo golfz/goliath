a0fa7048c8d0ae27de19db581f4083eae8495867
Surattikorn Chumkaew <surattikorn@gmail.com>
2020-06-08 17:06:20 +0700

    structMapper.To return output.GoliathError

--- structmapper.go
+++ structmapper.go
@@ -1,7 +1,12 @@
 package goliath
 
 import (
+	"fmt"
+	"github.com/golfz/goliath/x/data/output"
 	"github.com/jinzhu/copier"
+	"net/http"
+	"runtime/debug"
+	"time"
 )
 
 type structMapper struct {
@@ -17,9 +22,22 @@ func (s *structMapper) From(v interface{}) *structMapper {
 	return s
 }
 
-func (s *structMapper) To(v interface{}) error {
+func (s *structMapper) To(v interface{}) output.GoliathError {
 	if err := copier.Copy(v, s.from); err != nil {
-		return err
+		stack := string(debug.Stack())
+		errorStr := fmt.Sprint(err)
+		return &output.Error{
+			Status:   http.StatusInternalServerError,
+			Time:     time.Now(),
+			Type:     "internal",
+			Code:     "internal-mapstruct",
+			Error:    "Something wrong",
+			Message:  "Something wrong (cannot mapping data).",
+			ErrorDev: output.ErrorDev{
+				Error:      errorStr,
+				Stacktrace: stack,
+			},
+		}
 	}
 	return nil
 }
