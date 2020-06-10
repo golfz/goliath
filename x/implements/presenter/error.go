17c8c65220e1c42e5de5fde462ac51680e96ff74
golfz <surattikorn@gmail.com>
2020-06-10 15:17:01 +0700

    add error presenter

---
+++ x/implements/presenter/error.go
@@ -0,0 +1,20 @@
+package presenter
+
+import (
+	"github.com/golfz/goliath/x/data/output"
+	"github.com/golfz/goliath/x/data/viewmodel"
+	"github.com/golfz/goliath/x/interfaces/view/restful/json"
+)
+
+type ErrorPresenter struct {
+	View json.Writer
+}
+
+func (p *ErrorPresenter) PresentError(err output.GoliathError) {
+	h := viewmodel.HttpHeader{
+		StatusCode:    err.Errors().Status,
+		Authorization: nil,
+		ContentType:   nil,
+	}
+	p.View.Write(h, err)
+}
