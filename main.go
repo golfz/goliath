2b83c085bad226f8c5414c4a6c8df60c8890e1f5
Surattikorn <surattikorn@gmail.com>
2020-10-27 22:13:23 +0700

    init goliath cli

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	log.Println("Test Hello world")
+	args := os.Args[1:]
+
+	if len(args) == 0  {
+		printHelp()
+		return
+	}
+
+	//args[]
+}
+
+func printHelp() {
+	fmt.Println(`
+Goliath is a tool for help you to develop go project as a clean architecture
+
+Usage:
+	goliath <command> [arguments]
+
+The <command> are:
+	
+	create		create something 
+	update		update goliath project structure
+	version 	print Goliath version
+
+Use "goliath help <command>" for more information about a command.
+		`)
 }
