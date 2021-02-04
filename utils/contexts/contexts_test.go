d9bb95b7beb48b45114073a9ab1622e78836a8d9
Surattikorn <surattikorn@gmail.com>
2021-02-04 18:47:41 +0700

    add test Test_goliathContext_Auth

--- utils/contexts/contexts_test.go
+++ utils/contexts/contexts_test.go
@@ -1,52 +1,32 @@
 package contexts
 
 import (
-	"net/http"
-	"reflect"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestNewGoliathContext(t *testing.T) {
-	type args struct {
-		r *http.Request
-	}
-	tests := []struct {
-		name string
-		args args
-		want *goliathContext
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewGoliathContext(tt.args.r); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewGoliathContext() = %v, want %v", got, tt.want)
-			}
-		})
+	r := httptest.NewRequest("GET", "/", nil)
+	got := NewGoliathContext(r)
+	if got == nil {
+		t.Errorf("NewGoliathContext() want something, got nil")
 	}
 }
 
 func Test_goliathContext_Auth(t *testing.T) {
-	type fields struct {
-		request     *http.Request
-		authContext *authContext
+	r := httptest.NewRequest("GET", "/", nil)
+	ctx1 := NewGoliathContext(r)
+
+	auth1 := ctx1.Auth()
+	auth2 := ctx1.Auth()
+
+	if auth1 != auth2 {
+		t.Errorf("Auth() want same authContext object, got different")
 	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   *authContext
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ctx := &goliathContext{
-				request:     tt.fields.request,
-				authContext: tt.fields.authContext,
-			}
-			if got := ctx.Auth(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Auth() = %v, want %v", got, tt.want)
-			}
-		})
+
+	ctx2 := NewGoliathContext(r)
+	auth3 := ctx2.Auth()
+	if auth3 == auth1 {
+		t.Errorf("Auth() want different authContext object, got same")
 	}
 }
