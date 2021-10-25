fe8f9f5dd8f9781c74e9d6b11c8ace19a4778c77
Surattikorn <surattikorn@gmail.com>
2021-10-26 00:24:57 +0700

    add JSONRestfulView

--- v2/view.go
+++ v2/view.go
@@ -1,5 +1,21 @@
 package goliath
 
+import (
+	"encoding/json"
+)
+
 type Viewer interface {
 	Write(status int, data interface{})
+}
+
+type JSONRestfulView struct {
+	Ctx Goliath
+}
+
+func (v *JSONRestfulView) Write(status int, body interface{}) {
+	v.Ctx.Response().Header().Set("Content-Type", "application/json")
+	v.Ctx.Response().WriteHeader(status)
+	if body != nil {
+		json.NewEncoder(v.Ctx.Response()).Encode(body)
+	}
 }
