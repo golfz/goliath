733bf0753965b0aba9e472281db60a5fe642c34f
Surattikorn <surattikorn@gmail.com>
2021-10-26 01:04:52 +0700

    implement ErrorPresenter

--- v2/presenter.go
+++ v2/presenter.go
@@ -1,5 +1,23 @@
 package goliath
 
-type ErrorPresenter interface {
+import "encoding/json"
+
+type ErrorPresenterInterface interface {
 	PresentError(err Error)
+}
+
+type ErrorPresenter struct {
+	View Viewer
+}
+
+func (p *ErrorPresenter) PresentError(err Error) {
+	e := getGoliathError(err)
+	p.View.Write(e.Status, err)
+}
+
+func getGoliathError(errInf Error) goliathError {
+	b, _ := json.Marshal(errInf)
+	var gErr goliathError
+	_ = json.Unmarshal(b, &gErr)
+	return gErr
 }
